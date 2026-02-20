34748bf1227929ba8e678e4fe3abaac1e4239f0a
TolstenkovAnton <mintearilla@gmail.com>
2026-02-21 01:45:15 +0300

    rename router

--- routers/commands/__init__.py
+++ routers/commands/__init__.py
@@ -3,9 +3,9 @@ __all__ = ("router", )
 
 from aiogram import Router
 from .base_commands import router as base_commands_router
-from .user_commands import router as user_commands_router
+from .single_calc_commands import router as single_calc_commands_router
 from .estimate_commands import router as estimate_commands_router
 
 
 router = Router()
-router.include_routers(base_commands_router, user_commands_router, estimate_commands_router)
+router.include_routers(base_commands_router, single_calc_commands_router, estimate_commands_router)
