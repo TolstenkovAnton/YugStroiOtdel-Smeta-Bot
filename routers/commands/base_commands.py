d319841797f505efc491dc9225fb74bfb7876d40
TolstenkovAnton <kykyruska2004@gmail.com>
2026-02-21 02:38:35 +0300

    add pricing handle

--- routers/commands/base_commands.py
+++ routers/commands/base_commands.py
@@ -1,6 +1,9 @@
 from aiogram import F, Router
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
+from aiogram.enums import ChatAction
+from aiogram.exceptions import TelegramNetworkError
+
 
 from buttons.keyboard_utils import *
 
@@ -45,27 +48,18 @@ async def pricing_handle(message: Message):
             document=FSInputFile(
                 path='pricing_2026-02-13.pdf',
                 filename='actual_pricing.pdf',
-                reply_markup=keyboard_by_start(),
-            )
+            ),
+            reply_markup=keyboard_by_start(),
         )
     except TelegramNetworkError:
         await message.answer('Попробуйте запросить прайс-лист ещё раз.')
 
 
-@router.message(F.text == ButtonName.PRICING)
-@router.message(Command('pricing'))
-async def pricing_handle(message: Message):
-    try:
-        await message.bot.send_chat_action(
-            chat_id=message.chat.id,
-            action=ChatAction.UPLOAD_DOCUMENT,
-        )
-        await message.reply_document(
-            document=FSInputFile(
-                path='pricing_2026-02-13.pdf',
-                filename='actual_pricing.pdf',
-                reply_markup=keyboard_by_start(),
-            )
-        )
-    except TelegramNetworkError:
-        await message.answer('Попробуйте запросить прайс-лист ещё раз.')
+@router.message(F.text == ButtonName.COMPUTE)
+@router.message(Command('compute_unit'))
+async def compute_handle(message: Message):
+    text = 'Выберите опцию единичного подсчёта или создания сметы.'
+    await message.answer(
+        text=text,
+        reply_markup=keyboard_for_compute(),
+    )
